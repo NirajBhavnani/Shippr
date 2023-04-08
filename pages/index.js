4b1e07eb6c5bcc1603735c4b3cc12296e4c6860f
nbhavna <niraj.bhavnani@morningstar.com>
2023-04-08 15:50:50 +0530

    Connected with the sanity, created backend database and HeroBanner component

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => {
+import { client } from "@/lib/client";
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {/* Adding ? to handle null check */}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   );
+};
+
+// In react.js, we use useEffect to fetch data from API
+// Whereas in next.js, getServerSideProps is used for the same
+export const getServerSideProps = async () => {
+  const query = "*[_type == 'product']";
+  const products = await client.fetch(query);
+
+  const bannerQuery = "*[_type == 'banner']";
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
