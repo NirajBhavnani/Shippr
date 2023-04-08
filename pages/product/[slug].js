2e73f18b118806cc74837ebbdd65ebc4f7a768ef
nbhavna <niraj.bhavnani@morningstar.com>
2023-04-08 20:07:07 +0530

    Similar products section added

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,6 @@
 // slug is the unique identifier which belongs to each product
 // [slug] indicates that it is going to be dynamic e.g: /product/headphones, /product/speaker
-import React from "react";
+import React, { useState } from "react";
 import {
   AiOutlineMinus,
   AiOutlinePlus,
@@ -8,21 +8,33 @@ import {
   AiOutlineStar,
 } from "react-icons/ai";
 import { client, urlFor } from "@/lib/client";
+import { Product } from "@/components";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
           </div>
-          {/* <div className="small-images-container">
-            {image?.map((item, index) => (
-              <img src={urlFor(item)} className="" onMouseEnter="" />
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
             ))}
-          </div> */}
+          </div>
         </div>
         <div className="product-detail-desc">
           <h1>{name}</h1>
@@ -61,6 +73,20 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="buy-now" onClick="">
               Buy Now
             </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like:</h2>
+
+        {/* Marquee essentially stands for scrolling part */}
+        <div className="marquee">
+          {/* track class is added for auto-scrolling */}
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
